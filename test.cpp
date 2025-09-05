64499e07b15cd3006b56b138080c2c6481abbe5d
ConfusedG <drippychestercheetah02@gmail.com>
2025-09-05 08:57:22 -0400

    wow code thing haha

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int younes = 1;
+    double gabriel = 2.02;
+    float guh = 1.7f;
+    bool okay = false;
+    char character = 'c';
+    string helloWorld = "hello world!!";
+    cout<<helloWorld<<character<<guh<<gabriel<<younes;
+}
