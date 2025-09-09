77f86d3946d5bb9ca81c020c01a04d494ffbe76d
ConfusedG <drippychestercheetah02@gmail.com>
2025-09-09 08:56:42 -0400

    calculator'd again

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
     9/9/25
     remade that readline function from scratch because I was bored and finding a useable gui library is hard
 */
+
 #include <iostream>
 using namespace std;
 
@@ -18,6 +19,16 @@ int main() {
     string number1 = readLine("enter number 1: ");
     string number2 = readLine("enter number 2: ");
     double sum = stod(number1) + stod(number2);
-    string strSum = to_string(sum);
-    cout << "The sum of these numbers is "<< strSum;
+    double dif = stod(number1) - stod(number2);
+    double quot = stod(number1) / stod(number2);
+    double prod = stod(number1) * stod(number2);
+    int mod = stoi(number1) % stoi(number2);
+    double pInc = sum++;
+    double nInc = sum--;
+    double values[] = {sum, dif, quot, prod, mod, pInc, nInc};
+    string operators[] = {"sum", "difference", "quotient", "product", "modulo'd version of", "sum plus one", "sum minus one"};
+    
+    for (int i = 0; i < size(values); i++) {
+        cout << "The " << operators[i] << " of " << number1 << " and " << number2 << " is " << values[i] << "\n";
+    }
 }
