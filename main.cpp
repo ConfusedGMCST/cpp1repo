13e0f98d8abe2fa0eb68ad84ff8b6abb565b0bf0
ConfusedG <drippychestercheetah02@gmail.com>
2025-09-22 08:58:06 -0400

    randomNumberProgram
    yuh

--- main.cpp
+++ main.cpp
@@ -1,34 +1,23 @@
 /*
     Christian Haase
-    Calculator
-    9/9/25
-    remade that readline function from scratch because I was bored and finding a useable gui library is hard
+    random timmyWimmy program
+    9/15/25
 */
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-string readLine(string input) {
-    string output;
-    cout<<input;
-    cin>>output;
-    return output;
-}
-
 int main() {
-    string number1 = readLine("enter number 1: ");
-    string number2 = readLine("enter number 2: ");
-    double sum = stod(number1) + stod(number2);
-    double dif = stod(number1) - stod(number2);
-    double quot = stod(number1) / stod(number2);
-    double prod = stod(number1) * stod(number2);
-    int mod = stoi(number1) % stoi(number2);
-    double pInc = sum++;
-    double nInc = sum--;
-    double values[] = {sum, dif, quot, prod, mod, pInc, nInc};
-    string operators[] = {"sum", "difference", "quotient", "product", "modulo'd version of", "sum plus one", "sum minus one"};
-    
-    for (int i = 0; i < size(values); i++) {
-        cout << "The " << operators[i] << " of " << number1 << " and " << number2 << " is " << values[i] << "\n";
+    srand(static_cast<unsigned int>(time(nullptr))); //sets the seed for the rand int
+    int timmyWimmy = (rand()%6) + 1; //random number with range of 6<-->1
+    cout<<timmyWimmy<<endl;
+    if (timmyWimmy == 5) {
+        cout<<"hot dogs"<<endl;
+    } else if (timmyWimmy == 6) {
+        cout<<"corn dogs"<<endl;
+    } else {
+        cout<<"chili dogs"<<endl;
     }
 }
